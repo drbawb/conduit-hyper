31335468c6c1c56801eccd232b3fd51581cbb2f3
Robbie Straw <drbawb@fatalsyntax.com>
2017-02-07 09:45:43 -0600

    (deps) bump to hyper master & stub out impl.
    This creates a stub impl for conduit::Request that matches the new
    hyper::server::Request object. Request#body() will need more major work
    since it needs to line up w/ the new async IO API from hyper. For the
    most part everything else was a straight-forward translation (minus the
    now unused lifetime bounds.)

--- src/lib.rs
+++ src/lib.rs
@@ -6,21 +6,24 @@ extern crate unicase;
 #[macro_use]
 extern crate log;
 
-use hyper::server::{Request as HyperRequest, Listening, Response, Fresh};
-use hyper::version::HttpVersion;
-use hyper::method::Method;
-use hyper::header::{Host, ContentLength};
-use hyper::uri::RequestUri;
-use hyper::net::{HttpListener, HttpsListener, SslServer, NetworkListener};
-use hyper::status::StatusCode;
-use std::collections::HashMap;
-use std::net::{SocketAddr, ToSocketAddrs};
+// use hyper::{Method, Url};
+// use hyper::server::{Request as HyperRequest, Response};
+// use hyper::version::HttpVersion;
+// use hyper::header::{Host, ContentLength};
+// use hyper::net::{HttpListener, HttpsListener, SslServer, NetworkListener};
+// use hyper::status::StatusCode;
+// use std::collections::HashMap;
+// use std::str;
+
+use hyper::{HttpVersion, Method};
+use hyper::header::{Headers as HyperHeaders, Host, ContentLength};
+use hyper::server::{Request as HyperRequest, Response};
 use std::io::{self, Read, Cursor};
-use std::str;
+use std::net::{SocketAddr, ToSocketAddrs};
 use unicase::UniCase;
 
-struct Request<'a, 'b: 'a> {
-    request: HyperRequest<'a, 'b>,
+struct Request {
+    request: HyperRequest,
     scheme: conduit::Scheme,
     headers: Headers,
     extensions: conduit::Extensions,
@@ -36,13 +39,17 @@ fn ver(major: u64, minor: u64) -> semver::Version {
     }
 }
 
-impl<'a, 'b> conduit::Request for Request<'a, 'b> {
+impl conduit::Request for Request {
     fn http_version(&self) -> semver::Version {
-        match self.request.version {
+        match *self.request.version() {
             HttpVersion::Http09 => ver(0, 9),
             HttpVersion::Http10 => ver(1, 0),
             HttpVersion::Http11 => ver(1, 1),
-            HttpVersion::Http20 => ver(2, 0),
+            HttpVersion::H2 => ver(2, 0),
+            HttpVersion::H2c => ver(2,0),
+
+            // NOTE: non-exhaustive patterns: `__DontMatchMe` not covered
+            _ => unimplemented!(),
         }
     }
 
@@ -51,7 +58,7 @@ impl<'a, 'b> conduit::Request for Request<'a, 'b> {
     }
 
     fn method(&self) -> conduit::Method {
-        match self.request.method {
+        match *self.request.method() {
             Method::Connect => conduit::Method::Connect,
             Method::Delete => conduit::Method::Delete,
             Method::Get => conduit::Method::Get,
@@ -71,7 +78,7 @@ impl<'a, 'b> conduit::Request for Request<'a, 'b> {
     }
 
     fn host<'c>(&'c self) -> conduit::Host<'c> {
-        conduit::Host::Name(&self.request.headers.get::<Host>().unwrap().hostname)
+        conduit::Host::Name(&self.request.headers().get::<Host>().unwrap().hostname())
     }
 
     fn virtual_root(&self) -> Option<&str> {
@@ -79,34 +86,26 @@ impl<'a, 'b> conduit::Request for Request<'a, 'b> {
     }
 
     fn path(&self) -> &str {
-        match self.request.uri {
-            RequestUri::AbsolutePath(ref s) => s.split('?').next().unwrap(),
-            _ => panic!("unsupported request type"),
-        }
+        self.request.path()
     }
 
     fn query_string(&self) -> Option<&str> {
-        match self.request.uri {
-            RequestUri::AbsolutePath(ref s) => s.splitn(2, '?').nth(1),
-            _ => panic!("unsupported request type"),
-        }
+        self.request.query()
     }
 
     fn remote_addr(&self) -> SocketAddr {
-        self.request.remote_addr
+        *self.request.remote_addr()
     }
 
     fn content_length(&self) -> Option<u64> {
-        self.request.headers.get::<ContentLength>().map(|h| h.0)
+        self.request.headers().get::<ContentLength>().map(|h| h.0)
     }
 
     fn headers(&self) -> &conduit::Headers {
         &self.headers
     }
 
-    fn body(&mut self) -> &mut Read {
-        &mut self.request
-    }
+    fn body<'a>(&'a mut self) -> &'a mut Read { unimplemented!() }
 
     fn extensions(&self) -> &conduit::Extensions {
         &self.extensions
@@ -139,119 +138,119 @@ impl conduit::Headers for Headers {
     }
 }
 
-pub struct Server<L> {
-    server: hyper::Server<L>,
-    scheme: conduit::Scheme,
-}
-
-impl Server<HttpListener> {
-    pub fn http<T>(addr: T) -> hyper::Result<Server<HttpListener>>
-        where T: ToSocketAddrs
-    {
-        Ok(Server {
-            server: hyper::Server::http(addr)?,
-            scheme: conduit::Scheme::Http,
-        })
-    }
-}
-
-impl<S> Server<HttpsListener<S>>
-    where S: SslServer + Clone + Send
-{
-    pub fn https<T>(addr: T, ssl: S) -> hyper::Result<Server<HttpsListener<S>>>
-        where T: ToSocketAddrs,
-    {
-        Ok(Server {
-            server: hyper::Server::https(addr, ssl)?,
-            scheme: conduit::Scheme::Https,
-        })
-    }
-}
-
-impl<L> Server<L>
-    where L: NetworkListener + Send + 'static
-{
-    pub fn new(listener: L, scheme: conduit::Scheme) -> Server<L> {
-        Server {
-            server: hyper::Server::new(listener),
-            scheme: scheme,
-        }
-    }
-
-    /// Returns a mutable reference to the inner Hyper `Server`.
-    pub fn as_mut(&mut self) -> &mut hyper::Server<L> {
-        &mut self.server
-    }
-
-    pub fn handle<H>(self, handler: H) -> hyper::Result<Listening>
-        where H: conduit::Handler
-    {
-        let handler = Handler {
-            handler: handler,
-            scheme: self.scheme,
-        };
-        self.server.handle(handler)
-    }
-
-    pub fn handle_threads<H>(self, handler: H, threads: usize) -> hyper::Result<Listening>
-        where H: conduit::Handler
-    {
-        let handler = Handler {
-            handler: handler,
-            scheme: self.scheme,
-        };
-        self.server.handle_threads(handler, threads)
-    }
-}
-
-struct Handler<H> {
-    handler: H,
-    scheme: conduit::Scheme,
-}
-
-impl<H> hyper::server::Handler for Handler<H>
-    where H: conduit::Handler
-{
-    fn handle<'a, 'k>(&'a self, request: HyperRequest<'a, 'k>, mut response: Response<'a, Fresh>) {
-        let mut headers = HashMap::new();
-        for header in request.headers.iter() {
-            headers.entry(header.name().to_owned())
-                .or_insert_with(Vec::new)
-                .push(header.value_string());
-        }
-        let mut request = Request {
-            request: request,
-            scheme: self.scheme,
-            headers: Headers(headers.into_iter().collect()),
-            extensions: conduit::Extensions::new(),
-        };
-
-        let mut resp = match self.handler.call(&mut request) {
-            Ok(response) => response,
-            Err(e) => {
-                error!("Unhandled error: {}", e);
-                conduit::Response {
-                    status: (500, "Internal Server Error"),
-                    headers: HashMap::new(),
-                    body: Box::new(Cursor::new(e.to_string().into_bytes())),
-                }
-            }
-        };
-
-        *response.status_mut() = StatusCode::from_u16(resp.status.0 as u16);
-        for (key, value) in resp.headers {
-            let value = value.into_iter().map(|s| s.into_bytes()).collect();
-            response.headers_mut().set_raw(key, value);
-        }
-
-        if let Err(e) = respond(response, &mut resp.body) {
-            error!("Error sending response: {}", e);
-        }
-    }
-}
-
-fn respond<'a>(response: Response<'a, Fresh>, body: &mut Box<conduit::WriteBody + Send>) -> io::Result<()> {
-    let mut response = response.start()?;
-    body.write_body(&mut response)?;
-    response.end()
-}
+// pub struct Server<L> {
+//     server: hyper::Server<L>,
+//     scheme: conduit::Scheme,
+// }
+// 
+// impl Server<HttpListener> {
+//     pub fn http<T>(addr: T) -> hyper::Result<Server<HttpListener>>
+//         where T: ToSocketAddrs
+//     {
+//         Ok(Server {
+//             server: hyper::Server::http(addr)?,
+//             scheme: conduit::Scheme::Http,
+//         })
+//     }
+// }
+// 
+// impl<S> Server<HttpsListener<S>>
+//     where S: SslServer + Clone + Send
+// {
+//     pub fn https<T>(addr: T, ssl: S) -> hyper::Result<Server<HttpsListener<S>>>
+//         where T: ToSocketAddrs,
+//     {
+//         Ok(Server {
+//             server: hyper::Server::https(addr, ssl)?,
+//             scheme: conduit::Scheme::Https,
+//         })
+//     }
+// }
+// 
+// impl<L> Server<L>
+//     where L: NetworkListener + Send + 'static
+// {
+//     pub fn new(listener: L, scheme: conduit::Scheme) -> Server<L> {
+//         Server {
+//             server: hyper::Server::new(listener),
+//             scheme: scheme,
+//         }
+//     }
+// 
+//     /// Returns a mutable reference to the inner Hyper `Server`.
+//     pub fn as_mut(&mut self) -> &mut hyper::Server<L> {
+//         &mut self.server
+//     }
+// 
+//     pub fn handle<H>(self, handler: H) -> hyper::Result<Listening>
+//         where H: conduit::Handler
+//     {
+//         let handler = Handler {
+//             handler: handler,
+//             scheme: self.scheme,
+//         };
+//         self.server.handle(handler)
+//     }
+// 
+//     pub fn handle_threads<H>(self, handler: H, threads: usize) -> hyper::Result<Listening>
+//         where H: conduit::Handler
+//     {
+//         let handler = Handler {
+//             handler: handler,
+//             scheme: self.scheme,
+//         };
+//         self.server.handle_threads(handler, threads)
+//     }
+// }
+// 
+// struct Handler<H> {
+//     handler: H,
+//     scheme: conduit::Scheme,
+// }
+// 
+// impl<H> hyper::server::Handler for Handler<H>
+//     where H: conduit::Handler
+// {
+//     fn handle<'a, 'k>(&'a self, request: HyperRequest<'a, 'k>, mut response: Response<'a, Fresh>) {
+//         let mut headers = HashMap::new();
+//         for header in request.headers.iter() {
+//             headers.entry(header.name().to_owned())
+//                 .or_insert_with(Vec::new)
+//                 .push(header.value_string());
+//         }
+//         let mut request = Request {
+//             request: request,
+//             scheme: self.scheme,
+//             headers: Headers(headers.into_iter().collect()),
+//             extensions: conduit::Extensions::new(),
+//         };
+// 
+//         let mut resp = match self.handler.call(&mut request) {
+//             Ok(response) => response,
+//             Err(e) => {
+//                 error!("Unhandled error: {}", e);
+//                 conduit::Response {
+//                     status: (500, "Internal Server Error"),
+//                     headers: HashMap::new(),
+//                     body: Box::new(Cursor::new(e.to_string().into_bytes())),
+//                 }
+//             }
+//         };
+// 
+//         *response.status_mut() = StatusCode::from_u16(resp.status.0 as u16);
+//         for (key, value) in resp.headers {
+//             let value = value.into_iter().map(|s| s.into_bytes()).collect();
+//             response.headers_mut().set_raw(key, value);
+//         }
+// 
+//         if let Err(e) = respond(response, &mut resp.body) {
+//             error!("Error sending response: {}", e);
+//         }
+//     }
+// }
+// 
+// fn respond<'a>(response: Response<'a, Fresh>, body: &mut Box<conduit::WriteBody + Send>) -> io::Result<()> {
+//     let mut response = response.start()?;
+//     body.write_body(&mut response)?;
+//     response.end()
+// }
