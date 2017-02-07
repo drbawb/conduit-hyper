81fea49e16d9313b43b84f42edde760ed9c76c65
Robbie Straw <drbawb@fatalsyntax.com>
2017-02-07 12:39:22 -0600

    finish impl. hyper response future
    At the moment this just naively copies the response from conduit into a
    buffer and sends the whole thing to Hyper. This should instead take
    advantage of the body cursor to stream the response to hyper in chunks.

--- src/lib.rs
+++ src/lib.rs
@@ -7,23 +7,17 @@ extern crate unicase;
 #[macro_use]
 extern crate log;
 
-// use hyper::{Method, Url};
-// use hyper::server::{Request as HyperRequest, Response};
-// use hyper::version::HttpVersion;
-// use hyper::header::{Host, ContentLength};
-// use hyper::net::{HttpListener, HttpsListener, SslServer, NetworkListener};
-// use hyper::status::StatusCode;
-// use std::collections::HashMap;
-// use std::str;
-use std::sync::{Arc, RwLock};
-
-use futures::{Future, Poll};
+use futures::Future;
 use hyper::{HttpVersion, Method};
 use hyper::header::{Headers as HyperHeaders, Host, ContentLength};
 use hyper::server::{Request as HyperRequest, Response as HyperResponse};
 use hyper::server::{Http, NewService, Service};
+use hyper::status::StatusCode;
+use std::collections::HashMap;
 use std::io::{self, Read, Cursor};
 use std::net::{SocketAddr, ToSocketAddrs};
+use std::str;
+use std::sync::{Arc, RwLock};
 use unicase::UniCase;
 
 struct Request {
@@ -150,16 +144,17 @@ impl conduit::Headers for Headers {
 
 pub struct Server<H> {
     server: hyper::Server<ServiceAcceptor<H>>,
-    scheme: conduit::Scheme,
 }
 
 impl<H: conduit::Handler+'static> Server<H> {
     pub fn http(addr: SocketAddr, handler: H) -> hyper::error::Result<Server<H>> {
-        let acceptor = ServiceAcceptor::new(handler);
+        let acceptor = ServiceAcceptor {
+            handler: Arc::new(handler),
+            scheme: conduit::Scheme::Http,
+        };
 
         Ok(Server {
             server: Http::new().bind(&addr, acceptor)?,
-            scheme: conduit::Scheme::Http,
         })
     }
 
@@ -169,15 +164,18 @@ impl<H: conduit::Handler+'static> Server<H> {
 }
 
 /// Accepts incoming requests and attaches them to the conduit handler
-struct ServiceAcceptor<H> { handler: Arc<H> }
+struct ServiceAcceptor<H> { 
+    handler: Arc<H>,
+    scheme: conduit::Scheme,
+}
 
-// NOTE: puts handler behind an arc so that service futures can access it
-impl<H: conduit::Handler> ServiceAcceptor<H> {
-    /// Create a new server which will use this handler to transform
-    /// requests
-    pub fn new(handler: H) -> ServiceAcceptor<H> {
+// NOTE: not sure why, but #[derive(Clone)] is returning &Self for
+// this struct ... guess we'll do this ourselves.
+impl<H> Clone for ServiceAcceptor<H> {
+    fn clone(&self) -> Self {
         ServiceAcceptor {
-            handler: Arc::new(handler),
+            handler: self.handler.clone(),
+            scheme:  self.scheme,
         }
     }
 }
@@ -186,149 +184,69 @@ impl<H: conduit::Handler> NewService for ServiceAcceptor<H> {
     type Request  = HyperRequest;
     type Response = HyperResponse;
     type Error    = hyper::Error; // TODO: better error type?
-    type Instance = ServiceTerminator<H>;
+    type Instance = ServiceAcceptor<H>;
 
     fn new_service(&self) -> Result<Self::Instance, io::Error> {
-        let terminator = ServiceTerminator::new(self.handler.clone());
-        Ok(terminator)
+        Ok(self.clone())
     }
 }
 
-struct ServiceTerminator<H: 'static> { handler: Arc<H> }
-
-impl<H: conduit::Handler+'static> ServiceTerminator<H> {
-    pub fn new(handler: Arc<H>) -> ServiceTerminator<H> {
-        ServiceTerminator { handler: handler }
-    }
-}
-
-impl<H: conduit::Handler+'static> Service for ServiceTerminator<H> {
+impl<H: conduit::Handler+'static> Service for ServiceAcceptor<H> {
     type Request  = HyperRequest;
     type Response = HyperResponse;
     type Error    = hyper::Error; // TODO: better error type?
     type Future   = Box<Future<Item = Self::Response, Error = Self::Error>>;
 
-    fn call(&self, req: Self::Request) -> Self::Future {
-        let mut resp = Self::Response::new()
-            .with_body(String::from("hello conduit!").into_bytes());
+    fn call(&self, request: Self::Request) -> Self::Future {
+        let mut headers = HashMap::new();
+        for header in request.headers().iter() {
+            headers.entry(header.name().to_owned())
+                .or_insert_with(Vec::new)
+                .push(header.value_string());
+        }
 
-        futures::finished(resp).boxed()
+        let mut request = Request {
+            request: request,
+            scheme: self.scheme,
+            headers: Headers(headers.into_iter().collect()),
+            extensions: conduit::Extensions::new(),
+        };
+
+        let mut resp = match self.handler.call(&mut request) {
+            Ok(response) => response,
+            Err(e) => {
+                error!("Unhandled error: {}", e);
+                conduit::Response {
+                    status: (500, "Internal Server Error"),
+                    headers: HashMap::new(),
+                    body: Box::new(Cursor::new(e.to_string().into_bytes())),
+                }
+            }
+        };
+
+        let mut outgoing_headers = HyperHeaders::with_capacity(resp.headers.len());
+        for (key, value) in resp.headers {
+            let value: Vec<_> = value.into_iter().map(|s| s.into_bytes()).collect();
+            outgoing_headers.set_raw(key, value);
+        }
+
+        // TODO: take advantage of tokio to stream resp here?
+        let mut buf = vec![];
+        if let Err(e) = respond(&mut buf, &mut resp.body) {
+            error!("Error sending response: {:?}", e);    
+        };
+
+
+        let response = HyperResponse::new()
+            .with_status(StatusCode::from_u16(resp.status.0 as u16))
+            .with_headers(outgoing_headers)
+            .with_body(buf);
+
+        futures::finished(response).boxed()
     }
 }
 
-// pub struct Server<L> {
-//     server: hyper::Server<L>,
-//     scheme: conduit::Scheme,
-// }
-// 
-// impl Server<HttpListener> {
-//     pub fn http<T>(addr: T) -> hyper::Result<Server<HttpListener>>
-//         where T: ToSocketAddrs
-//     {
-//         Ok(Server {
-//             server: hyper::Http::bind(addr)?,
-//             scheme: conduit::Scheme::Http,
-//         })
-//     }
-// }
-// 
-// impl<S> Server<HttpsListener<S>>
-//     where S: SslServer + Clone + Send
-// {
-//     pub fn https<T>(addr: T, ssl: S) -> hyper::Result<Server<HttpsListener<S>>>
-//         where T: ToSocketAddrs,
-//     {
-//         Ok(Server {
-//             server: hyper::Server::https(addr, ssl)?,
-//             scheme: conduit::Scheme::Https,
-//         })
-//     }
-// }
-// 
-// impl<L> Server<L>
-//     where L: NetworkListener + Send + 'static
-// {
-//     pub fn new(listener: L, scheme: conduit::Scheme) -> Server<L> {
-//         Server {
-//             server: hyper::Server::new(listener),
-//             scheme: scheme,
-//         }
-//     }
-// 
-//     /// Returns a mutable reference to the inner Hyper `Server`.
-//     pub fn as_mut(&mut self) -> &mut hyper::Server<L> {
-//         &mut self.server
-//     }
-// 
-//     pub fn handle<H>(self, handler: H) -> hyper::Result<Listening>
-//         where H: conduit::Handler
-//     {
-//         let handler = Handler {
-//             handler: handler,
-//             scheme: self.scheme,
-//         };
-//         self.server.handle(handler)
-//     }
-// 
-//     pub fn handle_threads<H>(self, handler: H, threads: usize) -> hyper::Result<Listening>
-//         where H: conduit::Handler
-//     {
-//         let handler = Handler {
-//             handler: handler,
-//             scheme: self.scheme,
-//         };
-//         self.server.handle_threads(handler, threads)
-//     }
-// }
-// 
-// struct Handler<H> {
-//     handler: H,
-//     scheme: conduit::Scheme,
-// }
-// 
-// impl<H> hyper::server::Handler for Handler<H>
-//     where H: conduit::Handler
-// {
-//     fn handle<'a, 'k>(&'a self, request: HyperRequest<'a, 'k>, mut response: Response<'a, Fresh>) {
-//         let mut headers = HashMap::new();
-//         for header in request.headers.iter() {
-//             headers.entry(header.name().to_owned())
-//                 .or_insert_with(Vec::new)
-//                 .push(header.value_string());
-//         }
-//         let mut request = Request {
-//             request: request,
-//             scheme: self.scheme,
-//             headers: Headers(headers.into_iter().collect()),
-//             extensions: conduit::Extensions::new(),
-//         };
-// 
-//         let mut resp = match self.handler.call(&mut request) {
-//             Ok(response) => response,
-//             Err(e) => {
-//                 error!("Unhandled error: {}", e);
-//                 conduit::Response {
-//                     status: (500, "Internal Server Error"),
-//                     headers: HashMap::new(),
-//                     body: Box::new(Cursor::new(e.to_string().into_bytes())),
-//                 }
-//             }
-//         };
-// 
-//         *response.status_mut() = StatusCode::from_u16(resp.status.0 as u16);
-//         for (key, value) in resp.headers {
-//             let value = value.into_iter().map(|s| s.into_bytes()).collect();
-//             response.headers_mut().set_raw(key, value);
-//         }
-// 
-//         if let Err(e) = respond(response, &mut resp.body) {
-//             error!("Error sending response: {}", e);
-//         }
-//     }
-// }
-// 
-// fn respond<'a>(response: Response<'a, Fresh>, body: &mut Box<conduit::WriteBody + Send>) -> io::Result<()> {
-//     let mut response = response.start()?;
-//     body.write_body(&mut response)?;
-//     response.end()
-// }
+fn respond(response: &mut Vec<u8>, body: &mut Box<conduit::WriteBody + Send>) -> io::Result<()> {
+        body.write_body(response)?;
+        Ok(())
+}
